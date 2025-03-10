b998f692e5923b9405b788dc0ca62e9836f0b44d
최 성필 <seongpil0948@idstrust.com>
2025-03-10 10:41:50 +0900

    fix error

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,28 @@
-import typescript from "rollup-plugin-typescript2";
+import typescript from 'rollup-plugin-typescript2';
 
 // https://www.peterkimzz.com/rollupjs-using-plugin/
 export default {
-  input: ["lib/index.ts"],
+  input: ['lib/index.ts'],
   output: [
     {
-      dir: "dist",
-      entryFileNames: "[name].js",
+      dir: 'dist',
+      entryFileNames: '[name].js',
       // exports: "named",
     },
   ],
   plugins: [typescript()],
   external: [
-    "@firebase/firestore",
-    "@firebase/storage",
-    "@firebase/util",
-    "@firebase/auth",
-    "@firebase/analytics",
-    "firebase/app",
-    "date-fns",
-    "date-fns/locale",
-    "@firebase/messaging",
-    "@editorjs/editorjs",
+    '@firebase/firestore',
+    '@firebase/storage',
+    '@firebase/util',
+    '@firebase/auth',
+    '@firebase/analytics',
+    'firebase/app',
+    'date-fns',
+    'date-fns/locale',
+    '@firebase/messaging',
+    '@editorjs/editorjs',
+    'xlsx',
+    'fs',
   ],
 };
